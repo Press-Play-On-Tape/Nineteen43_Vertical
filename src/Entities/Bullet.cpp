7c8bbb6e09f1ea9fdb9c8d97369150a57a9ced82
Filmote <simon@bloggingadeadhorse.com>
2018-08-21 08:02:11 +1000

    WIP

--- src/Entities/Bullet.cpp
+++ src/Entities/Bullet.cpp
@@ -54,47 +54,51 @@ void Bullet::setEnabled(const bool value) {
 
 void Bullet::move() {
   
-  switch (getDirection()) {
+  const int8_t xOffset[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+  const int8_t yOffset[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+  // switch (getDirection()) {
   
-    case Direction::North:
-      --_y;  
-      break;
+  //   case Direction::North:
+  //     --_y;  
+  //     break;
   
-    case Direction::NorthEast:
-      ++_x;
-      --_y;
-      break;
+  //   case Direction::NorthEast:
+  //     ++_x;
+  //     --_y;
+  //     break;
   
-    case Direction::East:
-      ++_x;
-      break;
+  //   case Direction::East:
+  //     ++_x;
+  //     break;
   
-    case Direction::SouthEast:
-      ++_x;
-      ++_y;
-      break;
+  //   case Direction::SouthEast:
+  //     ++_x;
+  //     ++_y;
+  //     break;
   
-    case Direction::South:
-      ++_y;
-      break;
+  //   case Direction::South:
+  //     ++_y;
+  //     break;
   
-    case Direction::SouthWest:
-      --_x;
-      ++_y;
-      break;
+  //   case Direction::SouthWest:
+  //     --_x;
+  //     ++_y;
+  //     break;
   
-    case Direction::West:
-      --_x;
-      break;
+  //   case Direction::West:
+  //     --_x;
+  //     break;
   
-    case Direction::NorthWest:
-      --_x;
-      --_y;
-      break;
+  //   case Direction::NorthWest:
+  //     --_x;
+  //     --_y;
+  //     break;
 
-    default: break;
+  //   default: break;
       
-  }
+  // }
+  _x = _x + xOffset[(uint8_t)getDirection()];
+  _y = _y + yOffset[(uint8_t)getDirection()];
 
   if (_x > WIDTH - SCOREBOARD_OUTER_RECT_WIDTH) setEnabled(false);    
   if (_y > HEIGHT) setEnabled(false);
